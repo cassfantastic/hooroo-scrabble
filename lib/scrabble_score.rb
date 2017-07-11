79a267aa5bcb378512850ab78296330a0f0b6728
Cass Fantastic <cass.fantastic@gmail.com>
2017-07-11 17:50:01 +1000

    Fixed tests, added code to pass empty string and nil tests

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,4 +1,7 @@
 # lib/scrabble_score
 
 class ScrabbleScore 
+    def self.score(word)
+        return 0 if (word == "") or (word == nil)
+    end
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -4,22 +4,22 @@ describe ScrabbleScore do
     describe ".score" do
         context "given an empty string" do
             it "returns zero" do
-                expect(ScrabbleScore.score("").to eql(0))
+                expect(ScrabbleScore.score("")).to eql(0)
             end
         end
         context "given a nil input" do
             it "returns zero" do
-                expect(ScrabbleScore.score(nil).to eql(0))
+                expect(ScrabbleScore.score(nil)).to eql(0)
             end
         end
         context "given a string \"hello\"" do
             it "returns eight" do
-                expect(ScrabbleScore.score("hello").to eql(8))
+                expect(ScrabbleScore.score("hello")).to eql(8)
             end
         end
         context "given a string \"cabbage\"" do
             it "returns fourteen" do
-                expect(ScrabbleScore.score("cabbage").to eql(14))
+                expect(ScrabbleScore.score("cabbage")).to eql(14)
             end
         end
     end
